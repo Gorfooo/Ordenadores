4303e1d064959a2a5e12af4d85b121be97574439
Filipe Golfe <filipe_golfe@yahoo.com>
2021-05-04 21:48:31 -0300

    first commit

---
+++ heapSort.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/00a4711f34.js" crossorigin="anonymous"></script>
+    <title>Heap Sort</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark d-flex align-items-center">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id='navbarSupportedContent'>
+            <ul class='navbar-nav mr-auto'>
+                <li class='nav-item'><a class="navbar-brand text-white pr-2" href="#"
+                        onclick="window.open('bubbleSort.php','_self');">Bubble Sort</a></li>
+                <li class='nav-item'><a class="navbar-brand text-white pr-2" href="#"
+                        onclick="window.open('quickSort.php','_self');">Quick Sort</a></li>
+                <li class='nav-item'><a class="navbar-brand text-white pr-2" href="#">Heap Sort</a></li>
+                        <li class='nav-item'><a class="navbar-brand text-white pr-2" href="#"
+                        onclick="window.open('insertionSort.php','_self');">Insertion Sort</a></li>
+                <li class='nav-item'><a class="navbar-brand text-white pr-2" href="#"
+                onclick="window.open('shellSort.php','_self');">Shell Sort</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-sm-2'>
+                <table class="table mt-5 table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Números</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class='col-sm-2'>
+            <table class="table mt-5 table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Ordenados</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
